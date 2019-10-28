e9b1cd03c9a51e727dc965ab5392351b73b95c88
Andrea Capelli <426288+Valair@users.noreply.github.com>
2019-10-28 20:49:52 +0100

    Fix search input positioning

--- src/js/plugins/search-input.js
+++ src/js/plugins/search-input.js
@@ -1,3 +1,9 @@
 $(() => {
-    $('.top_controls div[piwik-quick-access=""]').insertBefore($('.top_controls span.icon.icon-arrowup'));
+    let lastElement = $('.top_controls span.icon.icon-arrowup');
+    let searchElement = $('.top_controls div[piwik-quick-access=""]');
+    if (lastElement.length) {
+        searchElement.insertBefore(lastElement);
+        return;
+    }
+    $('.top_controls').append(searchElement);
 });
